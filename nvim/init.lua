c7bf31ed20c318ba3ec708c9a078e1174accfca2
Aadi Waghray <aadiwaghray@gmail.com>
2024-04-04 02:37:49 -0400

    added pop-up window for diagonostics
    Source: https://www.reddit.com/r/neovim/comments/11np4x6/error_messages_getting_out_of_screen/

--- nvim/init.lua
+++ nvim/init.lua
@@ -529,3 +529,19 @@ require("headlines").setup {
         fat_headline_lower_string = "🬂",
     },
 }
+
+vim.diagnostic.config({
+    virtual_text = false,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = false,
+})
